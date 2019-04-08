5e0e30ca5f8ed22e3eea8ae3dadd692bec7301b0
Tiago Costa <tdcosta100@gmail.com>
2019-04-08 05:47:14 -0300

    Start button behavior
    Change start button behavior. Now it changes wallpaper style when clicked.

--- scripts/taskbar.js
+++ scripts/taskbar.js
@@ -1,5 +1,7 @@
 (function($)
 {
+	var wallpaperStyle;
+	
 	$("#start-button").on("mouseenter", function()
 	{
 		$(this).addClass("hover");
@@ -13,5 +15,42 @@
 	$("#start-button").on("click", function()
 	{
 		$(this).toggleClass("pressed");
+		
+		changeWallpaperStyle();
 	});
+	
+	function changeWallpaperStyle()
+	{
+		switch(wallpaperStyle)
+		{
+			case "Center":
+				wallpaperStyle = "Tile";
+				
+				$("#desktop").css("background-size", "auto");
+				$("#desktop").css("background-position", "left top");
+				$("#desktop").css("background-repeat", "repeat");
+				
+				break;
+			case "Tile":
+				wallpaperStyle = "Stretch";
+				
+				$("#desktop").css("background-size", "100% 100%");
+				$("#desktop").css("background-position", "left top");
+				$("#desktop").css("background-repeat", "no-repeat");
+				
+				break;
+			case "Stretch":
+			default:
+				wallpaperStyle = "Center";
+				
+				$("#desktop").css("background-size", "auto");
+				$("#desktop").css("background-position", "center center");
+				$("#desktop").css("background-repeat", "no-repeat");
+				break;
+		}
+		
+		console.log(wallpaperStyle);
+	}
+	
+	changeWallpaperStyle();
 })(jQuery);
