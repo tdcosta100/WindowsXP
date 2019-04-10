8b1c8f91b5b081d4f79f71d68c8c8988f6f4e0cd
Tiago Costa <tdcosta100@gmail.com>
2019-04-10 19:07:54 -0300

    Window - Work in progress

--- scripts/window.js
+++ scripts/window.js
@@ -5,23 +5,110 @@ https://github.com/tdcosta100
 
 (function($)
 {
-	$(document).on("mouseenter", ".close-button", function()
+	$(document).on("mouseenter", ".caption-button", function()
 	{
 		$(this).addClass("hover");
 	});
 	
-	$(document).on("mouseleave", ".close-button", function()
+	$(document).on("mouseleave", ".caption-button", function()
 	{
 		$(this).removeClass("hover pressed");
 	});
 	
-	$(document).on("mousedown", ".close-button", function()
+	$(document).on("mousedown", ".caption-button", function()
 	{
 		$(this).addClass("pressed");
 	});
 	
-	$(document).on("mouseup", ".close-button", function()
+	$(document).on("mouseup", ".caption-button", function()
 	{
 		$(this).removeClass("pressed");
+	});
+	
+	$(document).on("click", ".max-button", function()
+	{
+		var button = $(this);
+		var currentWindow = button.parents(".window");
+		
+		var animatedWindow = $("#templates .window").clone();
+		animatedWindow.addClass("animation");
+		animatedWindow.css(
+			currentWindow.css([ "left", "top", "right" ])
+		);
+		
+		$(".caption .background", animatedWindow).empty().removeClass("sliced");
+		$(".caption .container", animatedWindow).empty();
+		
+		$(".frame", animatedWindow).remove();
+
+		var content = $(".caption .container > .content", currentWindow).clone();
+		content.appendTo($(".caption .container", animatedWindow));
+		animatedWindow.appendTo("#desktop");
+		
+		animatedWindow.animate(
+			{
+				"left": 0,
+				"top": 0,
+				"right": 0
+			},
+			{
+				duration: 300,
+				complete: function()
+				{
+					animatedWindow.remove();
+					
+					currentWindow.addClass("maximized");
+					
+					button.removeClass("max-button").addClass("restore-button");
+					$(".overlay", button).attr("title", "Restaurar")
+				}
+			}
+		);
+	});
+	
+	$(document).on("click", ".restore-button", function()
+	{
+		var button = $(this);
+		var currentWindow = button.parents(".window");
+		
+		var measuringObject = $("<div/>");
+		measuringObject.attr("style", currentWindow.attr("style"));
+		
+		var targetValues = measuringObject.css([ "left", "top", "width" ]);
+		measuringObject.remove();
+		
+		var animatedWindow = $("#templates .window").clone();
+		animatedWindow.addClass("animation");
+		
+		animatedWindow.css({
+			"left": 0,
+			"top": 0,
+			"right": 0
+		});
+		
+		$(".caption .background", animatedWindow).empty().removeClass("sliced");
+		$(".caption .container", animatedWindow).empty();
+		
+		$(".frame", animatedWindow).remove();
+
+		var content = $(".caption .container > .content", currentWindow).clone();
+		content.appendTo($(".caption .container", animatedWindow));
+		animatedWindow.appendTo("#desktop");
+		
+		animatedWindow.animate(
+			targetValues,
+			{
+				duration: 300,
+				complete: function()
+				{
+					animatedWindow.remove();
+					
+					button.parents(".window").removeClass("maximized");
+					
+					button.removeClass("restore-button").addClass("max-button");
+					$(".overlay", button).attr("title", "Maximizar")
+				}
+			}
+		);
 	});
 })(jQuery);
